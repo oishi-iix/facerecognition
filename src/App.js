f961220eb389fd2ea9c637c61858cdeb234b8704
oishi-Xii-iiX <oiji2504@gmail.com>
2020-04-22 23:11:09 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 108,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+      console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+  render(){
+    return (
+      <div className="App">
+        <Particles className = 'particles'params={particlesOptions}/> 
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/*
+        
+        <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
